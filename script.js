74957a42d7eb7a237c97fd5828b95868207f9043
Void <stas050595@gmail.com>
2025-11-18 17:52:27 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const counter = document.querySelector(".counter");
 const button = document.querySelector(".mainbtn");
 const morePerClick = document.querySelector(".morePerClick");
 
-let money = -1;
-let amountPerClick = 1;
+let money = 0;
+let amountPerClick = 10;
 
 let amountPerClickLevels = {
     1: { cost: 25, amount: 2 },
