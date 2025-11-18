ed6695195fa2f05f0ab3502239cbfb22a3d69272
Void <stas050595@gmail.com>
2025-11-18 17:54:51 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const counter = document.querySelector(".counter");
 const button = document.querySelector(".mainbtn");
 const morePerClick = document.querySelector(".morePerClick");
+const morePerSecond = document.querySelector(".morePerSecond");
 
 let money = 0;
 let amountPerClick = 10;
@@ -10,12 +11,25 @@ let amountPerClickLevels = {
     2: { cost: 75, amount: 5 }
 };
 
+let amountPerSecondLevels = {
+    1: { cost: 50 , amount: 1},
+    2: { cost: 175 , amount: 4}
+};
+
 let currentAmountPerClickLevel = 0;
 const maxAmountPerClickLevel = 2;
+
+let moneyPerSecond = 0;
+let currentAmountPerSecondLevel = 0;
+const maxAmountPerSecondLevel = 2;
 
 function buttonClicked() {
     money += amountPerClick;
     counter.textContent = `money: ${money}`;
+}
+
+function wait(ms) {
+    return new Promise(resolve => setTimeout(resolve , ms));
 }
 
 button.addEventListener("click", buttonClicked);
@@ -32,9 +46,43 @@ morePerClick.addEventListener("click", () => {
 
         const upcoming = amountPerClickLevels[currentAmountPerClickLevel + 1];
         morePerClick.textContent = upcoming ? `more per click - ${upcoming.cost}$` : "MAX";
+        
+        if (morePerClick.textContent == "MAX") {
+            morePerClick.classList.add("max");
+        }
 
         counter.textContent = `money: ${money}`;
     }
 });
+
+morePerSecond.addEventListener("click", () => {
+    const nextLevel = currentAmountPerSecondLevel + 1;
+
+    if (nextLevel <= maxAmountPerSecondLevel && money >= amountPerSecondLevels[nextLevel].cost) {
+        money -= amountPerSecondLevels[nextLevel].cost;
+
+        moneyPerSecond += amountPerSecondLevels[nextLevel].amount;
+
+        currentAmountPerSecondLevel++;
+
+        const upcoming = amountPerSecondLevels[currentAmountPerSecondLevel + 1];
+        morePerSecond.textContent = upcoming ? `more per second - ${upcoming.cost}$` : "MAX";
+
+        if (morePerSecond.textContent == "MAX") {
+            morePerSecond.classList.add("max");
+        }
+
+        counter.textContent = `money: ${money}`;
+    }
+});
+
+async function afkLoop() {
+    money += moneyPerSecond;
+    counter.textContent = `money: ${money}`;
+    await wait(1000);
+    afkLoop();
+}
+
+afkLoop();
 
 buttonClicked();
